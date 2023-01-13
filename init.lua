7dbf37b6d3f72f9f76d99bfce4745cf0d269bbf0
MobSlicer152 <brambleclaw1414@gmail.com>
2023-01-13 08:55:43 -0500

    Fix syntax error

--- init.lua
+++ init.lua
@@ -44,7 +44,7 @@ autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
 autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
 autocmd InsertLeave * match ExtraWhitespace /\s\+$/
 autocmd BufWinLeave * call clearmatches()
-]]
+]],
     true)
 
 require("nvim-tree").setup()
